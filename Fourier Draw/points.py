0c71450e2f43ca4ba3036920598c3922ad771d7f
Ojaswy Akella <32218681+Ojaswy@users.noreply.github.com>
2019-08-04 21:31:32 +0530

    Update points.py

--- Fourier Draw/points.py
+++ Fourier Draw/points.py
@@ -7,7 +7,7 @@ from pyglet.window import mouse
 
 
 
-class Main(pyglet.window.Window):
+class Main(pyglet.window.Window): 
     def __init__ (self):
         super().__init__(640, 480)
         self.x_points = []
